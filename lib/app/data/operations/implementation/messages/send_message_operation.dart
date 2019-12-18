b3f71028fc9ec21484612f5b1dddc8d3a01e9c88
Valeriy Shtaits <bajiepkasmile@yandex.ru>
2019-12-18 22:45:18 +0300

    Add SendMessageOperation

---
+++ lib/app/data/operations/implementation/messages/send_message_operation.dart
@@ -0,0 +1,23 @@
+import 'package:yo_app/app/model/message_status.dart';
+
+import '../../../../../architecture/data/operation/operation.dart';
+import '../../../../model/message.dart';
+import '../../../../model/profile.dart';
+
+class SendMessageOperation extends Operation<SendMessageArg, Message> {
+  
+  @override
+  Future<Message> execute(SendMessageArg arg) =>
+      Future.delayed(
+          Duration(seconds: 5),
+          () => Message("", arg.message.senderProfileId, MessageStatus.SENT, "Yo", true, arg.message.timestamp)
+      );
+}
+
+class SendMessageArg {
+
+  final Profile contact;
+  final Message message;
+
+  SendMessageArg(this.contact, this.message);
+}
