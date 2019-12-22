3a2467fb626e29754ba136358ebe307d0c5f0d6c
Valeriy Shtaits <bajiepkasmile@yandex.ru>
2019-12-22 18:55:26 +0300

    Update OnInitReaction, YoAppScope

--- lib/app/presentation/app/reactions/on_init_reaction.dart
+++ lib/app/presentation/app/reactions/on_init_reaction.dart
@@ -1,12 +1,56 @@
+import '../../../../connectivity_utils/architecture/data/box/is_network_available_box.dart';
 import '../../../../architecture/presentation/reaction/reaction.dart';
+import '../../../data/repositories/self_profile_repository.dart';
+import '../../../data/settings/self_profile_setting.dart';
+import '../../../data/settings/uid_setting.dart';
+import '../../../data/tasks/get_remote_self_profile_task.dart';
 import '../../views/log_in/log_in_route.dart';
 
 class OnInitReaction extends Reaction<void> {
 
+  final SelfProfileRepository _selfProfileRepository;
+  final SelfProfileSetting _selfProfileSetting;
+  final UidSetting _uidSetting;
+  final IsNetworkAvailableBox _isNetworkAvailableBox;
+  final GetRemoteSelfProfileTask _getRemoteSelfProfileTask;
   final LogInRoute _logInRoute;
 
-  OnInitReaction(this._logInRoute);
+  OnInitReaction(
+      this._selfProfileRepository,
+      this._selfProfileSetting,
+      this._uidSetting,
+      this._isNetworkAvailableBox,
+      this._getRemoteSelfProfileTask,
+      this._logInRoute
+  );
 
   @override
-  void excite(void arg) => _logInRoute.follow(null);
+  void excite(void arg) async {
+    final cachedSelfProfile = await _selfProfileSetting.get();
+    if (cachedSelfProfile != null) {
+      _selfProfileRepository.data = cachedSelfProfile;
+      // TODO: ContactListRoute
+      await _getRemoteSelfProfileTask.execute(null);
+      return null;
+    }
+
+    final cachedUid = _uidSetting.get();
+    if (cachedUid == null) {
+      _logInRoute.follow(null);
+      return null;
+    }
+
+    final isNetworkAvailable = await _isNetworkAvailableBox.get();
+    if (!isNetworkAvailable) {
+      // TODO: no net error
+      return null;
+    }
+
+    final profile = _getRemoteSelfProfileTask.execute(null);
+    if (profile != null) {
+      // TODO: ContactListRoute
+    } else {
+      // TODO: ProfileEditorRoute
+    }
+  }
 }
--- lib/app/presentation/app/yo_app_scope.dart
+++ lib/app/presentation/app/yo_app_scope.dart
@@ -1,31 +1,95 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:connectivity/connectivity.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../../connectivity_utils/architecture/data/box/is_network_available_box.dart';
+import '../../../flutter_utils/architecture/presentation/scope/app_scope.dart';
+import '../../../flutter_utils/architecture/presentation/scope/scope_bundle.dart';
 import '../../../architecture/presentation/view/view_model.dart';
-import '../../../implemented_architecture/presentation/scope/app_scope.dart';
-import '../../../implemented_architecture/presentation/scope/scope_bundle.dart';
-import '../../data/operations/implementation/auth/confirm_log_in_operation.dart';
-import '../../data/operations/implementation/auth/log_in_operation.dart';
+import '../../data/operations/implementation/auth/verify_phone_operation.dart';
 import '../../data/operations/implementation/messages/get_remote_messages_operation.dart';
 import '../../data/operations/implementation/messages/send_message_operation.dart';
 import '../../data/repositories/self_profile_repository.dart';
+import '../../data/operations/implementation/auth/log_in_operation.dart';
+import '../../data/operations/implementation/profiles/get_remote_self_profile_operation.dart';
+import '../../data/operations/tools/firestore/firestore_entry.dart';
+import '../../data/operations/tools/firestore/profile/firestore_profile_mapper.dart';
+import '../../data/tasks/get_remote_self_profile_task.dart';
+import '../../data/tasks/log_in_task.dart';
+import '../../data/settings/self_profile_setting.dart';
+import '../../data/settings/uid_setting.dart';
 import '../views/log_in/log_in_route.dart';
 import 'reactions/on_init_reaction.dart';
 
 class YoAppScope extends AppScope<YoAppScope> {
 
+  FirebaseAuth firebaseAuth;
+  Firestore firestore;
+  Connectivity connectivity;
+  Future<SharedPreferences> prefsFuture;
+
+  FirestoreEntry firestoreEntry;
+  FirestoreProfileMapper firestoreProfileMapper;
+
+  VerifyPhoneOperation verifyPhoneOperation;
   LogInOperation logInOperation;
-  ConfirmLogInOperation confirmLogInOperation;
+  GetRemoteSelfProfileOperation getRemoteSelfProfileOperation;
   GetRemoteMessagesOperation getRemoteMessagesOperation;
   SendMessageOperation sendMessageOperation;
+
+  LogInTask logInTask;
+  GetRemoteSelfProfileTask getRemoteSelfProfileTask;
+
   SelfProfileRepository selfProfileRepository;
+
+  SelfProfileSetting selfProfileSetting;
+  UidSetting uidSetting;
+
+  IsNetworkAvailableBox isNetworkAvailableBox;
+
   LogInRoute logInRoute;
+
   OnInitReaction onInitReaction;
 
   YoAppScope(ScopeBundle<YoAppScope, void, void, ViewModel> bundle) : super(bundle) {
-    logInOperation = LogInOperation();
-    confirmLogInOperation = ConfirmLogInOperation();
+    firebaseAuth = FirebaseAuth.instance;
+    firestore = Firestore.instance;
+    connectivity = Connectivity();
+    prefsFuture = SharedPreferences.getInstance();
+
+    firestoreEntry = FirestoreEntry(firestore);
+    firestoreProfileMapper = FirestoreProfileMapper();
+
+    verifyPhoneOperation = VerifyPhoneOperation(firebaseAuth);
+    logInOperation = LogInOperation(firebaseAuth);
+    getRemoteSelfProfileOperation = GetRemoteSelfProfileOperation(firestoreEntry, uidSetting, firestoreProfileMapper);
     getRemoteMessagesOperation = GetRemoteMessagesOperation();
     sendMessageOperation = SendMessageOperation();
+
+    logInTask = LogInTask(logInOperation, uidSetting);
+    getRemoteSelfProfileTask = GetRemoteSelfProfileTask(
+        getRemoteSelfProfileOperation,
+        selfProfileSetting,
+        selfProfileRepository
+    );
+
     selfProfileRepository = SelfProfileRepository();
+
+    selfProfileSetting = SelfProfileSetting(prefsFuture);
+    uidSetting = UidSetting(prefsFuture);
+
+    isNetworkAvailableBox = IsNetworkAvailableBox(connectivity);
+
     logInRoute = LogInRoute(context, this, screenContainer);
-    onInitReaction = OnInitReaction(logInRoute);
+
+    onInitReaction = OnInitReaction(
+        selfProfileRepository,
+        selfProfileSetting,
+        uidSetting,
+        isNetworkAvailableBox,
+        getRemoteSelfProfileTask,
+        logInRoute
+    );
   }
 }
